03f447e27843581288baa4b41a34a2862e18bb28
ViYulton <brooks_sxj@foxmail.com>
2021-04-30 00:11:41 +0800

    remove unnecessary elements, improve UX

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 server = 'http://114.116.94.235/';
+
+function insert_option(form, select, value, content) {
+    var option = document.createElement('option');
+    option.innerText = content;
+    $(option).attr('value', value);
+    select.append(option);
+    form.render('select');
+}
