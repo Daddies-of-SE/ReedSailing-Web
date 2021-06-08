d32b070e5b13de98c016f464ad08b90b7ffeab45
brooks <brooks_sxj@foxmail.com>
2021-06-08 11:20:18 +0800

    fix slow loading for act add/edit

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ function insert_option(form, select, value, content) {
     option.innerText = content;
     $(option).attr('value', value);
     select.append(option);
-    form.render('select');
 }
 
 function time_check(start, end) {
